af27e9e6ecfd83aa0479a630948c2819c78c9a61
salmansiddiqui-99 <159806428+salmansiddiqui-99@users.noreply.github.com>
2025-12-24 21:36:02 +0500

    adding changes

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ai-physical-robotics-book.vercel.app',
+  url: 'https://hackathon1-orpin.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
